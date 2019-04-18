e542c13e13ba586096901bddf1a5142795e61c5b
Hari Priya Jupally <jharipriya@outlook.com>
2019-04-18 11:04:05 -0500

    commit

--- Hello, Drawing/MyView.swift
+++ Hello, Drawing/MyView.swift
@@ -13,15 +13,33 @@ class MyView: UIView {
 //     Only override draw() if you perform custom drawing.
 //     An empty implementation adversely affects performance during animation.
     
-    var points:[CGPoint] = []
+    var points:[[CGPoint]] = []
+    var colors:[UIColor] = []
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(erase(gr:)))
+        tapGR.numberOfTapsRequired = 2
+        self.addGestureRecognizer(tapGR)
+    }
+    
+    @objc func erase(gr:UITapGestureRecognizer) {
+        points = []
+        colors = []
+        setNeedsDisplay()
+    }
     
     override func draw(_ rect: CGRect) {
-        if points.count == 0 {
-            return
-        }
-        let path = UIBezierPath()
-        path.move(to: points[0])
-        for pt in points {
+        if points.count > 0 {
+            for i in 0..<points.count {
+                let subpath = points[i]
+                if subpath.count == 0 {
+                    return
+                }
+            let path = UIBezierPath()
+                colors[i].setStroke()
+            path.move(to: subpath[0])
+        for pt in subpath {
             path.addLine(to: pt)
         }
         path.stroke()
@@ -36,22 +54,25 @@ class MyView: UIView {
 //        path.stroke()
 //        path.fill()
     }
-    
+}
+    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
-        points.append((touch?.location(in: self))!)
-        print("Began: \(String(describing: touch?.location(in: self)))")
+        points.append([(touch?.location(in: self))!])
+        colors.append(UIColor(displayP3Red: CGFloat.random(in: 0.0...1.0), green: CGFloat.random(in: 0.0...1.0), blue: CGFloat.random(in: 0.0...1.0), alpha: 1.0))
+        //print("Began: \(String(describing: touch?.location(in: self)))")
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
-        points.append((touch?.location(in: self))!)
+        points[points.count-1].append((touch?.location(in: self))!)
         print("Moved: \(String(describing: touch?.location(in: self)))")
         setNeedsDisplay()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
+        points[points.count-1].append((touch?.location(in: self))!)
         print("Ended: \(String(describing: touch?.location(in: self)))")
     }
 
@@ -60,3 +81,4 @@ class MyView: UIView {
         print("Cancelled: \(String(describing: touch?.location(in: self)))")
     }
 }
+
